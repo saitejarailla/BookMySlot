c2e997a435bd2ad0e5e3bb441ea03b038f4bdadd
saiteja <saitejarailla@gmail.com>
2023-09-02 12:19:33 +0530

    responsive

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,28 +1,44 @@
-import React, { useState , useEffect} from 'react';
-import './styles/Calendar.css'; // Import your CSS file
-
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
+import React, { useState, useEffect } from "react";
+import "./styles/Calendar.css"; // Import your CSS file
+import Login from "./Login";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function Calendar({ loggedUser }) {
   // Function to generate an array of days in a month
 
-  const getDaysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
+  const getDaysInMonth = (year, month) =>
+    new Date(year, month + 1, 0).getDate();
 
   const months = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
-
+  const [AvailableSlots, setAvailableSlots] = useState(0);
   const timeSlots = [
-    '9:30 AM - 10:30 AM',
-    '10:30 AM - 11:30 AM',
-    '11:30 AM - 12:30 PM',
-    '12:30 PM - 1:30 PM',
-    '1:30 PM - 2:30 PM',
-    '2:30 PM - 3:30 PM',
+    "9:30 AM - 10:30 AM",
+    "10:30 AM - 11:30 AM",
+    "11:30 AM - 12:30 PM",
+    "12:30 PM - 1:30 PM",
+    "1:30 PM - 2:30 PM",
+    "2:30 PM - 3:30 PM",
     // ... Add more time slots as needed
+  ];
+  const yearsList = [
+    new Date().getFullYear(),
+    new Date().getFullYear() + 1,
+    new Date().getFullYear() + 2,
+    new Date().getFullYear() + 3,
   ];
 
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
@@ -32,6 +48,20 @@ function Calendar({ loggedUser }) {
   const [allBookedSlots, setAllBookedSlots] = useState(null);
   const [bookingUpdated, setBookingUpdated] = useState(false);
 
+
+  const getAvailableSlotsCount = (year, month, date) => {
+    if (allBookedSlots !== null) {
+      const bookedSlotsForDate = allBookedSlots.filter(
+        bookedSlot =>
+          bookedSlot.year === year &&
+          bookedSlot.month === month &&
+          bookedSlot.date === date
+      );
+      return timeSlots.length - bookedSlotsForDate.length>=0?timeSlots.length - bookedSlotsForDate.length:0;
+    }
+    return timeSlots.length; // Return maximum available slots if allBookedSlots is null
+  };
+
   useEffect(() => {
     fetch(`https://backend-ya1p.onrender.com/get-booking`)
       .then((response) => response.json())
@@ -40,14 +70,25 @@ function Calendar({ loggedUser }) {
         console.log(allBookedSlots);
       })
       .catch((error) => {
-        console.error('Error in getting booking data:', error);
+        console.error("Error in getting booking data:", error);
       });
   }, [bookingUpdated]);
 
   // Function to check if a date is before the current date
   const isDateDisabled = (year, month, day) => {
     const currentDate = new Date();
-    return new Date(year, month, day) < currentDate;
+    if (allBookedSlots !== null) {
+      const bookedSlotsForDate = allBookedSlots.filter(
+        (bookedSlot) =>
+          bookedSlot.year === year &&
+          bookedSlot.month === month+1 &&
+          bookedSlot.date === day
+      );
+      console.log(currentDate,month)
+      return new Date(year, month, day) < currentDate || bookedSlotsForDate.length === timeSlots.length;     
+    }
+    return false;
+
   };
 
   const [expandedDate, setExpandedDate] = useState(null);
@@ -71,7 +112,6 @@ function Calendar({ loggedUser }) {
       setSelectedSlot(null);
     }
   };
-  
 
   const handleSlotSelect = (slot) => {
     setSelectedSlot(slot);
@@ -79,9 +119,9 @@ function Calendar({ loggedUser }) {
 
   const isSlotBooked = (year, month, date, slot) => {
     if (allBookedSlots !== null) {
-      console.log('Checking:', year, month, date, slot);
+      console.log("Checking:", year, month, date, slot);
       return allBookedSlots.some(
-        bookedSlot =>
+        (bookedSlot) =>
           bookedSlot.year === year &&
           bookedSlot.month === month &&
           bookedSlot.date === date &&
@@ -90,186 +130,246 @@ function Calendar({ loggedUser }) {
     }
     return false; // Return false if allBookedSlots is null
   };
-  
-  
-const isDateFullyBooked = (year, month, date) => {
-  if (allBookedSlots !== null) {
-    const bookedSlotsForDate = allBookedSlots.filter(
-      bookedSlot =>
-        bookedSlot.year === year &&
-        bookedSlot.month === month &&
-        bookedSlot.date === date
-    );
-    return bookedSlotsForDate.length === timeSlots.length;
-  }
-  return false; // Return false if allBookedSlots is null
-};
 
-const isDatePartiallyBooked = (year, month, date) => {
-  if (allBookedSlots !== null) {
-    const bookedSlotsForDate = allBookedSlots.filter(
-      bookedSlot =>
-        bookedSlot.year === year &&
-        bookedSlot.month === month &&
-        bookedSlot.date === date
-    );
-    return bookedSlotsForDate.length > 0 && bookedSlotsForDate.length < timeSlots.length;
-  }
-  return false; // Return false if allBookedSlots is null
-};
+  const isDateFullyBooked = (year, month, date) => {
+    if (allBookedSlots !== null) {
+      const bookedSlotsForDate = allBookedSlots.filter(
+        (bookedSlot) =>
+          bookedSlot.year === year &&
+          bookedSlot.month === month &&
+          bookedSlot.date === date
+      );
+      return bookedSlotsForDate.length >=4 && bookedSlotsForDate.length !=6;
+     
+    }
+    return false; // Return false if allBookedSlots is null
+  };
 
+  const isDatePartiallyBooked = (year, month, date) => {
+    if (allBookedSlots !== null) {
+      const bookedSlotsForDate = allBookedSlots.filter(
+        (bookedSlot) =>
+          bookedSlot.year === year &&
+          bookedSlot.month === month &&
+          bookedSlot.date === date
+      );
+      return (
+        bookedSlotsForDate.length <4  &&
+        bookedSlotsForDate.length >0
+      );
+    }
+    return false; // Return false if allBookedSlots is null
+  };
+
+
+  const[buttonClicked,setButtonClicked] = useState(false);
   const handleBooking = () => {
+    setButtonClicked(true);
     if (selectedDate !== null && selectedSlot !== null) {
       const bookingData = {
         user: loggedUser,
         year: selectedYear,
         month: selectedMonth + 1,
         date: selectedDate,
-        slot: timeSlots.indexOf(selectedSlot) + 1 // Get the index of the selected slot
+        slot: timeSlots.indexOf(selectedSlot) + 1, // Get the index of the selected slot
       };
-  
+
       fetch(`https://backend-ya1p.onrender.com/post-booking`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(bookingData)
+        body: JSON.stringify(bookingData),
       })
-      .then(response => response.json())
-      .then(data => {
-        // Update allBookedSlots with the newly booked slot
-        setAllBookedSlots(prevBookedSlots => [
-          ...prevBookedSlots,
-          {
-            year: selectedYear,
-            month: selectedMonth + 1,
-            date: selectedDate,
-            slot: timeSlots.indexOf(selectedSlot) + 1
-          }
-        ]);
-        
-        setBookingUpdated(true);
-        console.log('Booking response:', data);
-        // Clear selected slot and date
-        setSelectedSlot(null);
-        setSelectedDate(null);
-        handleClose();
-      })
-      .catch(error => {
-        console.error('Error in booking:', error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          // Update allBookedSlots with the newly booked slot
+          setAllBookedSlots((prevBookedSlots) => [
+            ...prevBookedSlots,
+            {
+              year: selectedYear,
+              month: selectedMonth + 1,
+              date: selectedDate,
+              slot: timeSlots.indexOf(selectedSlot) + 1,
+            },
+          ]);
+
+          setBookingUpdated(true);
+          console.log("Booking response:", data);
+          
+          // Clear selected slot and date
+          setSelectedSlot(null);
+          setSelectedDate(null);
+          handleClose();
+          setButtonClicked(false);
+        })
+        .catch((error) => {
+          console.error("Error in booking:", error);
+        });
     }
   };
-  
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => {
     setShow(false);
     setSelectedDate(null);
-
-  }
+  };
   const handleShow = () => setShow(true);
 
-  console.log(allBookedSlots)
+  console.log(allBookedSlots);
 
   return (
     <>
-  <>
-      {/* <Button variant="primary" onClick={handleShow}>
+      <>
+        {/* <Button variant="primary" onClick={handleShow}>
         Launch static backdrop modal
       </Button> */}
 
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Select a Time Slot on {expandedDate}/{selectedMonth + 1}/{selectedYear}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <div className="slot-options">
-            {timeSlots.map((slot, index) => (
-               <label key={index} className={`slot-option ${isSlotBooked(selectedYear, selectedMonth+1, expandedDate, index+1) ? 'disabled' : ''}`}>
-                <input
-                  type="radio"
-                  name="timeSlot"
-                  value={slot}
-                  checked={selectedSlot === slot}
-                  onChange={() => handleSlotSelect(slot)}
-                  disabled={isSlotBooked(selectedYear, selectedMonth+1, expandedDate, index+1)}
-                />
-                {slot}
-              </label>
-            ))}
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          {selectedSlot !== null && (
-            <Button className="book-button" onClick={handleBooking}>
-              Book Slot
-            </Button>
-          )}
-        </Modal.Footer>
-      </Modal>
-    </>
-    {loggedUser ? (
-    <div className="calendar-container">
-      <div className={`calendar ${expandedDate !== null ? 'expanded' : ''}`}>
-        <div className="calendar-header">
-        <div className="year-selector">
-              <label htmlFor="year">Select Year: </label>
-              <input
-                type="number"
-                id="year"
-                value={selectedYear}
-                onChange={(e) => setSelectedYear(parseInt(e.target.value))}
-              />
-            </div>
-          <div className="controls">
-          
-            {months.map((month, index) => (
-              <div
-                key={index}
-                className={`month-cell ${index === selectedMonth ? 'selected' : ''}`}
-                onClick={() => handleMonthClick(index)}
-              >
-                {month}
-              </div>
-            ))}
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Select a Time Slot on {expandedDate}/{selectedMonth + 1}/
+              {selectedYear}
+            </Modal.Title>:
             
-          </div>
-        </div>
-        <div className="date-grid">
-          {[...Array(getDaysInMonth(selectedYear, selectedMonth)).keys()].map(day => (
-            <div
-            key={day}
-            className={`date-cell
-              ${day + 1 === selectedDate ? 'selected' : ''}
-              ${isDateDisabled(selectedYear, selectedMonth, day + 1) ? 'disabled' : ''}
-              ${isDateFullyBooked(selectedYear, selectedMonth + 1, day + 1) ? 'fully-booked' : ''}
-              ${isDatePartiallyBooked(selectedYear, selectedMonth + 1, day + 1) && !isDateDisabled(selectedYear, selectedMonth, day + 1) ? 'partially-booked' : ''}
-            `}
-            onClick={() => handleDateClick(day + 1)}
+          </Modal.Header>
+          <Modal.Body>
+            <div className="slot-options">
+              {timeSlots.map((slot, index) => (
+                <label
+                  key={index}
+                  className={`slot-option ${
+                    isSlotBooked(
+                      selectedYear,
+                      selectedMonth + 1,
+                      expandedDate,
+                      index + 1
+                    )
+                      ? "disabled"
+                      : ""
+                  }`}
+                >
+                  <input
+                    type="radio"
+                    name="timeSlot"
+                    value={slot}
+                    checked={selectedSlot === slot}
+                    onChange={() => handleSlotSelect(slot)}
+                    disabled={isSlotBooked(
+                      selectedYear,
+                      selectedMonth + 1,
+                      expandedDate,
+                      index + 1
+                    )}
+                  />
+                  {slot}
+                </label>
+              ))}
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            {selectedSlot !== null && (
+              <Button className="book-button" onClick={handleBooking} disabled = {buttonClicked}>
+                Book Slot
+              </Button>
+            )}
+          </Modal.Footer>
+        </Modal>
+        
+
+        
+
+      </>
+      {loggedUser ? (
+        <div className="calendar-container">
+          <div
+            className={`calendar ${expandedDate !== null ? "expanded" : ""}`}
           >
-            {day + 1}
+            <div className="calendar-header">
+              <div className="year-selector">
+                <label htmlFor="year">Select Year: </label>
+                <select
+                  id="year"
+                  value={selectedYear}
+                  onChange={(e) => setSelectedYear(parseInt(e.target.value))}
+                >
+                  {yearsList.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="controls">
+                {months.map((month, index) => (
+                  <div
+                    key={index}
+                    className={`month-cell ${
+                      index === selectedMonth ? "selected" : ""
+                    }`}
+                    onClick={() => handleMonthClick(index)}
+                  >
+                    {month}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="date-grid">
+              {[
+                ...Array(getDaysInMonth(selectedYear, selectedMonth)).keys(),
+              ].map((day) => (
+                <div
+                  key={day}
+                  className={`date-cell
+              ${day + 1 === selectedDate ? "selected" : ""}
+              ${
+                isDateDisabled(selectedYear, selectedMonth, day + 1)
+                  ? "disabled"
+                  : ""
+              }
+              ${
+                isDateFullyBooked(selectedYear, selectedMonth + 1, day + 1)
+                  ? "fully-booked"
+                  : ""
+              }
+              ${
+                isDatePartiallyBooked(
+                  selectedYear,
+                  selectedMonth + 1,
+                  day + 1
+                ) && !isDateDisabled(selectedYear, selectedMonth, day + 1)
+                  ? "partially-booked"
+                  : ""
+              }
+            `}
+                  onClick={() => handleDateClick(day + 1)}
+                >
+                  {day + 1}<br></br>
+                  {isDateDisabled(selectedYear,selectedMonth,day+1)?<p>Closed</p>:(
+                  <p>Available: {getAvailableSlotsCount(selectedYear,selectedMonth+1,day+1)}</p>)}
+                </div>
+              ))}
+            </div>
           </div>
-          ))}
+          <div>
+
+          </div>
         </div>
-      </div>
-    </div>
-    )
-    :
-    (
-      <div>
-        Login to access Calendar
-      </div>
-    )
-  }
+      ) : (
+        
+        <div>
+          <h3>Login to start booking</h3>
+          <Login></Login></div>
+      )}
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,31 @@
+// HomePage.js
+
 import React from 'react';
 import './styles/Home.css';
-import homePageImage from './images/book appoinment.png';
+import homePageImage from './images/calender.jpg';
 import { Link } from 'react-router-dom';
 
 const HomePage = ({ loggedUser }) => {
   return (
     <div className="home-page">
-      
-      <div className="content">
-        <img
-          className="main-img"
-          src={homePageImage}
-          alt="Home"
-        />
-        <div className="text-container">
-          <h1 className="title">BookMySlot</h1>
-          <p className="description">Experience the future of hassle-free scheduling with our website, BookMySlot. No more endless emails or phone calls to book appointments. BookMySlot offers secure login, intuitive calendar views, and quick slot reservations, helping you manage your time effortlessly. Whether for work or personal needs, BookMySlot ensures you never miss a beat. Simplify your scheduling today!</p>
-          {
-            (loggedUser) 
-            ? 
-            <Link to='/calender'> <button className='getStartedButton' >Start Booking</button></Link> 
-            :
-            <Link to='/signup'> <button className='getStartedButton' >Get Started</button></Link> 
-          }
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="image-container">
+              <img className="main-img img-fluid w-100" src={homePageImage} alt="Home" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="text-container">
+              <h1 className="title">BookMySlot</h1>
+              <p className="description">Discover the seamless scheduling experience of our platform, BookMySlot. Say goodbye to the days of endless email exchanges or time-consuming phone calls to arrange appointments. BookMySlot provides a secure login, user-friendly calendar views, and swift slot reservations, streamlining your time management effortlessly. Whether for professional or personal commitments, BookMySlot guarantees you stay organized and never miss an important moment. Simplify your scheduling process today!</p>
+              {loggedUser ? (
+                <Link to='/calender'><button className='getStartedButton'>Start Booking</button></Link>
+              ) : (
+                <Link to='/signup'><button className='getStartedButton'>Get Started</button></Link>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
